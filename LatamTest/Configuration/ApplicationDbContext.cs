7ed3f87666a961505f73f3d37433101be44d00cf
Jonathan Gonzalez <jon.gon1@gmail.com>
2017-07-20 00:24:04 -0400

    First Steps resolving

---
+++ LatamTest/Configuration/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using LatamTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LatamTest.Configuration
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("LatamConnection")
+        {
+            //Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public virtual IDbSet<Product> Products { get; set; }
+    }
+}
---
+++ LatamTest/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LatamTest.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public int Number { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
